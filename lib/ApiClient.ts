7603c26b31988cdf32afebf3e1aeec20e35715cd
Adithya Kashyap Pulipaka <adithya2205@gmail.com>
2024-01-21 01:20:17 -0500

    fix base url

--- lib/ApiClient.ts
+++ lib/ApiClient.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosResponse } from "axios";
 
-const BASE_URL = process.env.BASE_URL || `http://localhost:3000/api`;
+const BASE_URL = process.env.BASE_URL || `/api`;
 
 export async function getAllPlans() {
   try {
